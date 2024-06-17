3fc53de440b520b5a658bf9ada256281831386f8
gabrielchua <gabriel@zumvet.com>
2024-06-17 23:14:43 +0800

    backend commit

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+1. cd into the backend folder
+2. install libraries
+
+    pip install -r requirements.txt
+3. cd into any microservice folder and run the file. Taking user service as an example:
+    cd user_service
+    python user.py
+4. [DOCKERFILE]: pending
---
+++ backend/carpark_service/carpark.py
@@ -0,0 +1,147 @@
+import os, sys, json
+from flask import Flask, request, jsonify
+from os import environ
+from flask_cors import CORS
+from pydantic import BaseModel
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from geojson import Point
+from geojson_pydantic import Feature, FeatureCollection, Point
+
+app = Flask(__name__)
+
+CORS(app)
+
+uri = environ.get("uri")
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db=client['DriveJieJie']
+carpark_collection=db["carpark"]
+
+class Parking(BaseModel):
+    period: str
+    fee: float
+    per_min_charge: float
+
+class Carpark(BaseModel):
+    name: str
+    address: str
+    location: Point
+    """
+    eg. location: {
+                    type: "Point",
+                    coordinates: [-73.856077, 40.848447]
+                    }<-- this whole thing is a Point object
+    """
+    parking_rate: list[Parking]
+  
+
+@app.route("/")
+def health():
+    return jsonify({"code": 200, "message": "Carpark service running"}), 200
+
+@app.route("/createcarparkloc", methods=['POST'])
+def create_tip():
+    """
+    use this end point to create tips to be stored in db
+    """
+    try:
+        data = request.get_json()
+        carpark_insert=Carpark(name=data.get('name'),
+                        address=data.get('address'),
+                        location=data.get('location'),
+                        parking_rate=[]
+                        ).model_dump()
+        rates=data.get('parkingRate')
+        for i in rates:
+            carpark_insert['parking_rate'].append(Parking(period=i['period'],
+                                                              fee=i['fee'],
+                                                              per_min_charge=i['perMinCharge']).model_dump())
+
+        result = carpark_collection.insert_one(carpark_insert)
+        print(result)
+        return jsonify(
+            {
+                "code": 201,
+                "data": request.get_json(),
+                "message": "Event has been registered in the ticket database",
+            }
+        ), 201
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        ex_str = (
+            str(e)
+            + " at "
+            + str(exc_type)
+            + ": "
+            + fname
+            + ": line "
+            + str(exc_tb.tb_lineno)
+        )
+        print(ex_str)
+
+        return jsonify(
+            {
+                "code": 400,
+                "data": request.get_json(),
+                "message": "An error occurred in carpark." ,
+            }
+        ), 400
+    
+
+@app.route("/getcarpark")
+def get_tips():
+    """
+    Use this endpoint to pull carparks from the db based on address, or part of it
+    below is the structure to sue for the payload
+    {"search":"<address>"} 
+    """
+    try:
+        ret=[]
+        data = request.get_json()
+        address = data.get("search")
+        result = carpark_collection.find({'address':{'$regex': f'.*{address}.*'}})
+        for i in result:
+            i["_id"]=None
+            ret.append(i)
+        
+        return jsonify(
+            {
+                "code": 200,
+                "data": ret,
+                "message": "carpark found",
+            }
+        ), 201
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        ex_str = (
+            str(e)
+            + " at "
+            + str(exc_type)
+            + ": "
+            + fname
+            + ": line "
+            + str(exc_tb.tb_lineno)
+        )
+        print(ex_str)
+
+        return jsonify(
+            {
+                "code": 400,
+                "data": request.get_json(),
+                "message": "An error occurred in carpark." ,
+            }
+        ), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8002, debug=False)
