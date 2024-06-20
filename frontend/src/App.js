1bcca66d390ad782c4b9b097639156c705e9eb16
JJDaBee <141636983+JJDaBee@users.noreply.github.com>
2024-06-20 20:29:21 +0800

    menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,61 @@
-import React from 'react'; 
-import { useState } from 'react'; 
-import { Button } from '@mui/material'; 
- 
-// import components 
-import HamburgerMenu from './components/HamburgerMenu.jsx'; 
-import MapComponent from './components/Map.jsx'; 
-import GasStationDialog from './components/GasStationDialog.jsx'; 
- 
-// import css 
-import './App.css'; 
- 
-function App() { 
+import React from 'react';
+import { useState } from 'react';
+import { Button } from '@mui/material';
 
-  // MUI component custom styling (button)
-  const buttonStyles = {
-    backgroundColor: 'var(--primary-color)',
-    borderRadius: 'var(--border-radius)',
-    color: 'white',
-    textTransform: 'none',
-    fontFamily: 'var(--font-family)',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-    marginLeft: '10px',
-  };
- 
-  // gas station dialog 
-  const [open, setOpen] = useState(false); 
-  const handleClickOpen = () => { 
-    setOpen(true); 
-  }; 
-  const handleClose = () => { 
-    setOpen(false); 
-  }; 
- 
-  return ( 
-    <div className="App"> 
- 
-      <div className="menu-bar"> 
-        <HamburgerMenu className="hamburger-menu"/> 
-        <Button sx={buttonStyles} variant="contained">Carparks</Button> 
-        <Button sx={buttonStyles} variant="contained" onClick={handleClickOpen}>Gas stations</Button> 
-        <Button sx={buttonStyles} variant="contained">Vehicle services</Button> 
-      </div> 
- 
-      <MapComponent /> 
- 
-      <GasStationDialog open={open} onClose={handleClose} /> 
-    </div> 
-  ); 
-} 
- 
+// import components
+import HamburgerMenu from './components/HamburgerMenu.jsx';
+import MapComponent from './components/Map.jsx';
+import GasStationDialog from './components/GasStationDialog.jsx';
+import Sidebar from './components/Menu.jsx';
+
+// import css
+import './App.css';
+
+function App() {
+    // MUI component custom styling (button)
+    const buttonStyles = {
+        backgroundColor: 'var(--primary-color)',
+        borderRadius: 'var(--border-radius)',
+        color: 'white',
+        textTransform: 'none',
+        fontFamily: 'var(--font-family)',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+        marginLeft: '10px',
+    };
+
+    // gas station dialog
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className="App">
+            <div className="menu-bar">
+                <HamburgerMenu className="hamburger-menu" />
+                <Button sx={buttonStyles} variant="contained">
+                    Carparks
+                </Button>
+                <Button
+                    sx={buttonStyles}
+                    variant="contained"
+                    onClick={handleClickOpen}
+                >
+                    Gas stations
+                </Button>
+                <Button sx={buttonStyles} variant="contained">
+                    Vehicle services
+                </Button>
+            </div>
+
+            <MapComponent />
+
+            <GasStationDialog open={open} onClose={handleClose} />
+        </div>
+    );
+}
+
 export default App;
--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -1,19 +1,67 @@
-import React from 'react';
-import './Sidebar.css';
+import React, { useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 
-function Sidebar() {
-  return (
-    <div className="sidebar">
-      <div className="sidebar-header">
-        <div className="profile-pic"></div>
-        <h2>Hi, khairyo!</h2>
-        <p>sushimallows8@gmail.com</p>
-      </div>
-      <ul className="sidebar-menu">
-        <li className="sidebar-item active">Tips</li>
-      </ul>
-    </div>
-  );
+export default function Sidebar() {
+    const [open, setOpen] = useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+        if (
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+        setOpen(open);
+    };
+
+    return (
+        <div>
+            <Paper
+                className="hamburger-menu"
+                sx={{
+                    p: '2px 4px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    width: 43,
+                    borderRadius: '30px',
+                }}
+            >
+                <IconButton
+                    sx={{ p: '10px' }}
+                    aria-label="menu"
+                    onClick={toggleDrawer(true)}
+                >
+                    <MenuIcon />
+                </IconButton>
+            </Paper>
+            <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
+                <div
+                    role="presentation"
+                    onClick={toggleDrawer(false)}
+                    onKeyDown={toggleDrawer(false)}
+                >
+                    <List>
+                        <ListItem>
+                            <div className="sidebar-header">
+                                <div className="profile-pic"></div>
+                                <h2>Hi, khairyo!</h2>
+                                <p>sushimallows8@gmail.com</p>
+                            </div>
+                        </ListItem>
+                        <Divider />
+                        <ListItem button>
+                            <ListItemText primary="Tips" />
+                        </ListItem>
+                    </List>
+                </div>
+            </Drawer>
+        </div>
+    );
 }
-
-export default Sidebar;
