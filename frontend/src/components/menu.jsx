1dd4b6ededa08dc1c168549b698c0f109e3ea6b2
JJDaBee <141636983+JJDaBee@users.noreply.github.com>
2024-06-26 15:22:20 +0800

    Changes to UI
    - icon and name added
    - avatar image added
    - still hardcoded**

--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -2,18 +2,19 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 import MenuIcon from '@mui/icons-material/Menu';
 import Paper from '@mui/material/Paper';
 import IconButton from '@mui/material/IconButton';
+import iconImage from '../images/drivejiejie-logo-blue.png';
 
 export default function HamburgerMenu() {
     const [open, setOpen] = React.useState(false);
@@ -24,41 +25,82 @@ export default function HamburgerMenu() {
 
     const DrawerList = (
         <Box
-            sx={{ width: 250 }}
+            sx={{ width: 280 }}
             role="presentation"
             onClick={toggleDrawer(false)}
         >
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map(
-                    (text, index) => (
+            <Box
+                sx={{
+                    backgroundColor: 'yellow',
+                    // padding: '16px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Box display="flex" alignItems="center" mb={2}>
+                    <img
+                        src={iconImage}
+                        alt="icon"
+                        style={{
+                            width: 61.1,
+                            height: 43.2,
+                            borderRadius: '50%',
+                            marginRight: '16px',
+                            marginTop: '20px',
+                        }}
+                    />
+                    <Typography
+                        variant="h6"
+                        mb={1}
+                        sx={{
+                            textAlign: 'center',
+                            marginRight: '65px',
+                            marginTop: '20px',
+                        }}
+                    >
+                        <span style={{ color: 'blue' }}>drive</span>
+                        <span style={{ color: 'black' }}>JieJie</span>
+                    </Typography>
+                </Box>
+            </Box>
+            <Box>
+                <Box
+                    sx={{
+                        backgroundColor: 'white',
+                        padding: '16px',
+                        borderRadius: '8px',
+                    }}
+                    display="flex"
+                    alignItems="center"
+                    mb={1}
+                >
+                    <Avatar sx={{ bgcolor: 'grey' }}>A</Avatar>
+                    <Box ml={2}>
+                        <Typography variant="body1">Hi, khairyo!</Typography>
+                        <Typography variant="body2" color="textSecondary">
+                            sushimallows8@gmail.com
+                        </Typography>
+                    </Box>
+                </Box>
+            </Box>
+            <Divider
+                sx={{
+                    backgroundColor: 'yellow',
+                    height: '2px',
+                    width: '80%',
+                    ml: 'auto',
+                }}
+            />
+            <Box sx={{ backgroundColor: 'white', padding: '16px' }}>
+                <List>
+                    {['Tips'].map((text) => (
                         <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    {index % 2 === 0 ? (
-                                        <InboxIcon />
-                                    ) : (
-                                        <MailIcon />
-                                    )}
-                                </ListItemIcon>
-                                <ListItemText primary={text} />
-                            </ListItemButton>
-                        </ListItem>
-                    )
-                )}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
                             <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
+                        </ListItem>
+                    ))}
+                </List>
+            </Box>
         </Box>
     );
 
